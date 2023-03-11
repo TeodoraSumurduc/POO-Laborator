9592ad89c14e0396857a3196a9db5b2b0b31f9bc
Teodora Sumurduc <126513009+TeodoraSumurduc@users.noreply.github.com>
2023-03-11 23:41:26 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -15,7 +15,7 @@ public:
         this->pret=prod.pret;
     }
 
-    Produs(const char *Denumire=NULL,int Pret=10){
+    Produs(char *Denumire=NULL,int Pret=10){
         if(Denumire!=NULL)
         {this->denumire=new char[strlen(Denumire)];
         strcpy(denumire,Denumire);}
